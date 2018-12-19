31ced2efd47d752b49534d1acda96562b81899c5
unknown <jacklee>
2018-12-19 15:55:30 +0800

    change sum to y

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,11 +1,11 @@
 public class HelloNumbers{
 	public static void main(String[] args){
 		int x=0;
-		int sum=0;
+		int y=0;
 		while(x<10){
-			System.out.print(sum+" ");
+			System.out.print(y+" ");
 			x=x+1;
-			sum=sum+x;
+			y=y+x;
 		}
 	}
 }
