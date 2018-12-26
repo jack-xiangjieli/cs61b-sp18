24fb4feae47505d6b86776b42d3abde9ab4dfe2b
unknown <jacklee>
2018-12-26 11:23:27 +0800

    debug arraydeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,118 +1,161 @@
+/** ArrayDeque is the deque that uses
+ * array as the core data structure.
+ * @author DELL nerdlee
+ * @since 2018/12/26
+ * @version 1.0
+ */
 public class ArrayDeque<T> {
+    /* an instance variable that caches the size of the array. */
     private int size;
-    private int nextfirst;
-    private int nextlast;
+    /* instance variable that points to the index of
+       a position that will store the next first item. */
+    private int nextfirst = 0;
+
+    /* instance variable that points to the index of
+      a position that will store the next last item. */
+    private int nextlast = 1;
+
+    /* declare an array that will be the basic data structure. */
     private T[] a;
 
-    public ArrayDeque(){
-        a=(T[])new Object[8];
-        size=0;
-        nextfirst=0;
-        nextlast=1;
+    /* constructor of an empty ArrayDeque. */
+    public ArrayDeque() {
+        a = (T[])new Object[8];
+        size = 0;
     }
 
-    public int size(){
+    /** method that returns the size of the ArrayDeque. */
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
-        return size==0;
+    /** method that checks whether the deque is empty. */
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    private void resize( ){
-        T[] b=(T[])new Object[a.length+1];
+    /** method that resize the deque if the size gets larger
+    than the length of the array.
+     */
+    private void resize( ) {
+        T[] b = (T[])new Object[a.length+1];
         System.arraycopy(a,0,b,0,nextlast);
-        if(nextlast<=size-1) {
-            System.arraycopy(a, nextlast + 2, 0, nextlast + 3, size - nextlast);
+        if (nextlast <= size - 1) {
+            int sp = nextlast + 2;
+            int dp = nextlast + 3;
+            int len = size - nextlast;
+            System.arraycopy(a, sp, b, dp, len);
         }
-        a=b;
+        a = b;
     }
-    public void addFirst(T item){
-        if(size>=a.length-2){
+
+    /** method that adds an item in front of the
+    previous ArrayDeque.
+     @param item  the item to be added to the ArrayDeque.
+     */
+    public void addFirst(T item) {
+        if(size >= a.length - 2) {
             resize();
-            if (nextfirst!=0) {
+            if (nextfirst != 0) {
                 nextfirst += 1;
             }
         }
-        a[nextfirst]=item;
-        if (nextfirst==0) {
+        a[nextfirst] = item;
+        if (nextfirst == 0) {
             nextfirst = a.length - 1;
-        } else {nextfirst=nextfirst-1;}
+        } else {nextfirst = nextfirst - 1;}
 
-        size+=1;
+        size += 1;
     }
 
-    public void addLast(T item){
-        if(size>=a.length-2) {
+    /** method that adds an item in the end of
+    the previous ArrayDeque.
+    @param item  the item to be added to the ArrayDeque.
+     */
+    public void addLast(T item) {
+        if (size >= a.length - 2) {
             resize();
             if (nextlast != size + 1) {
                 nextfirst += 1;
             }
         }
-        a[nextlast]=item;
-        if (nextlast==a.length-1){
-            nextlast=0;
-        }else {nextlast+=1;}
+        a[nextlast] = item;
+        if (nextlast == a.length - 1) {
+            nextlast = 0;
+        }else {nextlast += 1;}
 
-        size+=1;
+        size += 1;
     }
 
-    public T removeFirst(){
-        if(size==0){
+    /** method that removes the first item
+    of the ArrayDeque and output the item removed.
+     */
+    public T removeFirst() {
+        if (size == 0){
             return null;
         }
         T output;
-        if (nextfirst==a.length-1){
-            output=a[0];
-            a[0]=null;
-            nextfirst=0;
+        if (nextfirst == a.length - 1) {
+            output = a[0];
+            a[0] = null;
+            nextfirst = 0;
         } else {
             output = a[nextfirst + 1];
             a[nextfirst + 1] = null;
             nextfirst += 1;
         }
-        size-=1;
+        size -= 1;
         return output;
     }
 
-    public T removeLast(){
-        if(size==0){
+    /** method that removes the last item
+    of the ArrayDeque and output the item removed.
+     */
+    public T removeLast() {
+        if(size == 0){
             return null;
         }
         T output;
-        if (nextlast==0){
-            output=a[a.length-1];
-            a[a.length-1]=null;
-            nextlast=a.length-1;
+        if (nextlast == 0) {
+            output = a[a.length - 1];
+            a[a.length - 1] = null;
+            nextlast = a.length - 1;
         } else {
             output = a[nextlast - 1];
             a[nextlast - 1] = null;
-            nextfirst -= 1;
+            nextlast -= 1;
         }
-        size-=1;
+        size -= 1;
         return output;
     }
 
-    public void printDeque(){
-        int index=nextfirst+1;
-        for (int i=0; i<size;i++){
-            if (index==a.length){
-                index=0;
+    /** method that prints the item of
+    the ArrayDeque by order.
+     */
+    public void printDeque() {
+        int index = nextfirst + 1;
+        for (int i = 0; i<size; i++){
+            if (index == a.length) {
+                index = 0;
             }
             System.out.print(a[index]+" ");
-            index+=1;
+            index += 1;
         }
     }
 
-    public T get(int index){
-        if(index>size-1){
+    /** method that get the item of a
+    ArrayDeque given the index.
+     @param index the index of the item in the ArrayDeque.
+     */
+    public T get(int index) {
+        if(index > size-1) {
             return null;
         }
-        else if (index<=a.length-nextfirst-2){
-            return a[index+nextfirst+1];
+        else if (index <= a.length-nextfirst - 2) {
+            return a[index + nextfirst + 1];
         }
-        else{
-            return a[index+nextfirst+1-a.length];
+        else {
+            return a[index + nextfirst + 1 - a.length];
         }
     }
 
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -2,7 +2,65 @@
  * @author nerdlee
  * @since 2018/12/22
  */
+import org.junit.Test;
+import org.junit.Assert;
 
 public class ArrayDequeTest {
-    /*
+    @Test
+    public void isEmptyTest() {
+        ArrayDeque<String> ld = new ArrayDeque();
+        boolean actual = ld.isEmpty();
+        boolean expected = true;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addFirstSizeTest() {
+        ArrayDeque<String> ld=new ArrayDeque();
+        String a="a";
+        for (int i=0; i<16; i++) {
+            ld.addFirst("a"+i);
+        }
+        int actual=ld.size();
+        Assert.assertEquals(16,actual);
+    }
+
+    @Test
+    public void addLastSizeTest() {
+        ArrayDeque<String> ld=new ArrayDeque();
+        String a="a";
+        for (int i=0; i<16; i++) {
+            ld.addFirst("a"+i);
+        }
+        ld.addLast("b");
+        int actual=ld.size();
+        Assert.assertEquals(17,actual);
+    }
+
+    @Test
+    public void removeTest(){
+        ArrayDeque<String> ld=new ArrayDeque();
+        String a="a";
+        for (int i=0; i<16; i++) {
+            ld.addFirst("a"+i);
+        }
+        ld.addLast("b");
+        ld.removeFirst();
+        ld.removeLast();
+        int actual=ld.size();
+        Assert.assertEquals(15,actual);
+    }
+
+    @Test
+    public void getTest(){
+        ArrayDeque<String> ld=new ArrayDeque();
+        String a="a";
+        for (int i=0; i<16; i++) {
+            ld.addFirst("a"+i);
+        }
+        ld.addLast("b");
+        ld.removeFirst();
+        ld.removeLast();
+        Assert.assertEquals("a13",ld.get(6));
+    }
 }
