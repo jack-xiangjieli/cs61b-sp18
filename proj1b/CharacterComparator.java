8f38c39356f391c0035225820dd322bae344ee7d
unknown <jacklee>
2019-01-01 20:33:07 +0800

    change style

--- proj1b/CharacterComparator.java
+++ proj1b/CharacterComparator.java
@@ -1,5 +1,12 @@
-/** This interface defines a method for determining equality of characters. */
+/** This interface defines a method for determining
+ *  equality of characters.
+ * @author DELL
+ * */
 public interface CharacterComparator {
-    /** Returns true if characters are equal by the rules of the implementing class. */
+    /** Returns true if characters are equal by the rules of the
+     * implementing class.
+     * @param x input char
+     * @param y input char
+     * */
     boolean equalChars(char x, char y);
 }
--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -4,12 +4,42 @@
  * @since 2019/1/1
  */
 public interface Deque<T> {
+    /**  add an item
+      * @param item to add
+     */
     void addFirst(T item);
+
+    /** add an item.
+     * @param item to add
+     */
     void addLast(T item);
+
+    /** check whether is empty.
+     * @return true if empty
+     */
     boolean isEmpty();
+
+    /** return the size.
+     * @return the size
+     */
     int size();
+
+    /** print. */
     void printDeque();
+
+    /** remove the first item.
+     * @return item to be removed
+     */
     T removeFirst();
+
+    /** remove the last item
+     * @return item to be removed
+     */
     T removeLast();
+
+    /** get the item given the index
+     * @param index the index of the item
+     * @return the item
+     */
     T get(int index);
 }
